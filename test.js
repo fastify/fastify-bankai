c8253cb6721930b5274f7a59501b902676802f93
delvedor <tommydelved@gmail.com>
2017-05-22 12:51:35 +0200

    Updated test

---
+++ test.js
@@ -0,0 +1,104 @@
+'use strict'
+
+const t = require('tap')
+const test = t.test
+const fs = require('fs')
+const Fastify = require('fastify')
+const html = fs.readFileSync('./index.html', 'utf8')
+
+test('Should expose a route with the assets', t => {
+  t.plan(9)
+
+  const fastify = Fastify()
+  fastify.register(
+    require('./index'),
+    { entryFile: './client.js' }
+  )
+
+  fastify.inject({
+    url: '/',
+    method: 'GET'
+  }, res => {
+    t.equal(res.statusCode, 200)
+    t.equal(res.payload, html)
+    t.equal(res.headers['content-type'], 'text/html')
+  })
+
+  fastify.inject({
+    url: '/index.html',
+    method: 'GET'
+  }, res => {
+    t.equal(res.statusCode, 200)
+    t.equal(res.headers['content-type'], 'text/html')
+  })
+
+  fastify.inject({
+    url: '/bundle.css',
+    method: 'GET'
+  }, res => {
+    t.equal(res.statusCode, 200)
+    t.equal(res.headers['content-type'], 'text/css')
+  })
+
+  fastify.inject({
+    url: '/bundle.js',
+    method: 'GET'
+  }, res => {
+    t.equal(res.statusCode, 200)
+    t.equal(res.headers['content-type'], 'text/javascript')
+  })
+})
+
+test('should handle a base url', t => {
+  t.plan(2)
+
+  const fastify = Fastify()
+  fastify.register(
+    require('./index'),
+    { entryFile: './client.js', baseURL: '/test' }
+  )
+
+  fastify.inject({
+    url: '/test',
+    method: 'GET'
+  }, res => {
+    t.equal(res.statusCode, 200)
+    t.equal(res.payload, html)
+  })
+})
+
+test('should handle a given html file', t => {
+  t.plan(2)
+
+  const fastify = Fastify()
+  fastify.register(
+    require('./index'),
+    { entryFile: './client.js', html: './index.html' }
+  )
+
+  fastify.inject({
+    url: '/',
+    method: 'GET'
+  }, res => {
+    t.equal(res.statusCode, 200)
+    t.equal(res.payload, html)
+  })
+})
+
+test('should return 404 if an assets is not found', t => {
+  t.plan(1)
+
+  const fastify = Fastify()
+  fastify.register(
+    require('./index'),
+    { entryFile: './client.js' }
+  )
+
+  fastify.inject({
+    url: '/file.php',
+    method: 'GET'
+  }, res => {
+    t.equal(res.statusCode, 404)
+    process.exit(0)
+  })
+})

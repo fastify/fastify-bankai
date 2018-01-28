579fe39442bccce6720d1bbf64ab57236244e637
Matteo Collina <hello@matteocollina.com>
2018-01-28 09:27:59 +0800

    Updated

--- index.js
+++ index.js
@@ -5,9 +5,9 @@ const resolve = require('path').resolve
 const bankai = require('bankai')
 const fp = require('fastify-plugin')
 
-function assetsCompiler (fastify, opts, next) {
+function fastifyBankai (fastify, opts, next) {
   if (!opts.entry) {
-    return next(new Error('Missing entry file!'))
+    return next(new Error('missing entry file'))
   }
   if (typeof opts.entry !== 'string') {
     return next(new Error('entry must be a string'))
@@ -20,40 +20,94 @@ function assetsCompiler (fastify, opts, next) {
   delete opts.prefix
   const html = !!opts.html
   const htmlPath = resolve(opts.html || '')
-  const assets = bankai(resolve(opts.entry || ''), opts)
+  const compiler = bankai(resolve(opts.entry), opts)
 
   if (prefix && prefix.indexOf('/') !== 0) {
     prefix = '/' + prefix
   }
 
-  fastify.get(`${prefix || '/'}`, (req, reply) => {
-    reply
-      .header('Content-Type', 'text/html')
-      .send(html ? createReadStream(htmlPath) : assets.html(req.req, reply.res))
-  })
+  const rootRoute = `${prefix || '/'}`
+
+  // TODO is this needed?
+  if (html) {
+    fastify.get(rootRoute, (req, reply) => {
+      reply
+        .header('Content-Type', 'text/html')
+        .send(createReadStream(htmlPath))
+    })
+  } else {
+    fastify.get(rootRoute, (req, reply) => {
+      compiler.documents('/', (err, data) => {
+        if (err) {
+          reply.send(err)
+          return
+        }
+        reply
+          .header('content-type', 'text/html')
+          .send(data.buffer)
+      })
+    })
+  }
 
   fastify.get(`${prefix}/:file`, (req, reply) => {
-    switch (req.params.file.split('.').pop()) {
+    const [file, ext] = req.params.file.split('.')
+    switch (ext) {
       case 'html':
-        return reply
-          .header('content-type', 'text/html')
-          .send(assets.html(req.req, reply.res))
-      case 'css':
-        return reply
-          .header('content-type', 'text/css')
-          .send(assets.css(req.req, reply.res))
+        compiler.documents(req.req.url, (err, data) => {
+          if (err) {
+            reply.send(err)
+            return
+          }
+          reply
+            .header('content-type', 'text/html')
+            .send(data)
+        })
+        break
       case 'js':
-        return reply
-          .header('content-type', 'text/javascript')
-          .send(assets.js(req.req, reply.res))
+        compiler.scripts(file, (err, data) => {
+          if (err) {
+            reply.send(err)
+            return
+          }
+          reply
+            .header('content-type', 'application/javascript')
+            .send(data.buffer)
+        })
+        break
+      case 'css':
+        compiler.styles(file, (err, data) => {
+          if (err) {
+            reply.send(err)
+            return
+          }
+          reply
+            .header('content-type', 'text/css')
+            .send(data.buffer)
+        })
+        break
       default:
-        return reply
-          .code(404)
-          .send(new Error('Asset not found'))
+        compiler.assets(req.req.url, (err, data) => {
+          if (err) {
+            return reply
+              .code(404)
+              .send(err)
+          }
+
+          // TODO set the correct mime type?
+          reply.send(data.buffer)
+        })
     }
   })
 
-  next()
+  fastify.addHook('onClose', (f, done) => {
+    compiler.close()
+    setImmediate(done)
+  })
+
+  compiler.once('change', () => { next() })
 }
 
-module.exports = fp(assetsCompiler, '>= 0.27.0')
+module.exports = fp(fastifyBankai, {
+  name: 'fastify-bankai',
+  fastify: '>= 0.40.0'
+})
